667b83135dbf28183176206458c7eec93c7d0e73
Ewelina Mężyk <ewelinamezykmail@gmail.com>
2021-11-30 16:24:21 +0000

    game timer

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import { clickLogo } from "./components/logo/logo";
+import { time } from "./components/timer/timer";
+
 
 const app = () => {
   clickLogo();
+  time();
+
 };
 
 export default app;
