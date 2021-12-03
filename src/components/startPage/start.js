1ef8ea5a512c798ae02eb7ab2eea366cf62ac1c5
Dawid <Dawid.dolezol@o2.pl>
2021-12-03 11:21:53 +0100

    Desktop version of qioz main page. Need to add mobile, and some hover states and links in buttons.

---
+++ src/components/startPage/start.js
@@ -0,0 +1,99 @@
+////[[.................. Creating HTML structure............................]]////
+
+const body = document.querySelector("body");
+const main = document.createElement("main");
+body.appendChild(main);
+
+//creating left section
+const siteSection = document.createElement("div");
+siteSection.classList.add("siteSection");
+main.appendChild(siteSection);
+const buttonContainer = document.createElement("div");
+
+///Creating left section inside elements
+
+//creating logo
+
+const img = document.createElement("img");
+siteSection.appendChild(img);
+img.classList.add("logo");
+img.setAttribute(
+  "src",
+  "/src/components/startPage/img/toppng 7.png"
+);
+img.setAttribute("alt", "Game of Thrones logo");
+
+//creating poster
+
+const img2 = document.createElement("img");
+siteSection.appendChild(img2);
+img2.classList.add("poster");
+img2.setAttribute(
+  "src",
+  "/src/components/startPage/img/poster.png"
+);
+img2.setAttribute("alt", "Game of Thrones Poster");
+
+//creating option round buttons
+buttonContainer.classList.add("buttonContainer");
+siteSection.appendChild(buttonContainer);
+
+const roundButton = ["settings", "info", "fame"];
+const divs = roundButton.map((buttonClass) => {
+  const div = document.createElement("div");
+  div.classList.add(buttonClass);
+  return div;
+});
+
+buttonContainer.append(...divs);
+const setButton = document.querySelector(".settings");
+const infoButton = document.querySelector(".info");
+const fame = document.querySelector(".fame");
+const imgS = document.createElement("img");
+imgS.classList.add("imgs");
+setButton.appendChild(imgS);
+imgS.setAttribute(
+  "src",
+  "/src/components/startPage/img/settings.png"
+);
+
+const imgI = document.createElement("img");
+imgI.classList.add("imgI");
+infoButton.appendChild(imgI);
+imgI.setAttribute(
+  "src",
+  "/src/components/startPage/img/question.png"
+);
+
+const imgF = document.createElement("img");
+imgF.classList.add("imgF");
+fame.appendChild(imgF);
+imgF.setAttribute(
+  "src",
+  "/src/components/startPage/img/fame.png"
+);
+
+//creating game menu
+
+const containerGame = document.createElement("div");
+containerGame.classList.add("container");
+main.appendChild(containerGame);
+
+const choose = document.createElement("div");
+choose.classList.add("choose");
+choose.innerHTML = "Choose your quiz";
+const btn1 = document.createElement("button");
+btn1.classList.add("selection");
+btn1.innerHTML = "Character-name";
+const btn2 = document.createElement("button");
+btn2.classList.add("selection");
+btn2.innerHTML = "Name-character";
+const btn3 = document.createElement("button");
+btn3.classList.add("selection");
+btn3.innerHTML = "Character-family";
+const btnStart = document.createElement("button");
+btnStart.classList.add("start");
+btnStart.innerHTML = "Start";
+containerGame.append(choose, btn1, btn2, btn3, btnStart);
+const link1 = document.createElement("a");
+btn1.append(link1);
