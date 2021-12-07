f94cd24be2015f3d581098b686260a60f67335ce
Dawid <Dawid.dolezol@o2.pl>
2021-12-07 20:24:09 +0100

    Added some responsivity, replace hover state for very subtle, added function disabling active states from buttons when chhose another.

--- src/components/gameMenu.js
+++ src/components/gameMenu.js
@@ -1,7 +1,6 @@
 //creating game menu
 
 export const menuCreator = () => {
-  
   const choose = document.createElement("div");
   choose.classList.add("choose");
   choose.innerHTML = "Choose your quiz";
@@ -14,16 +13,15 @@ export const menuCreator = () => {
   const btn3 = document.createElement("button");
   btn3.classList.add("selection");
   btn3.innerHTML = "Character-family";
-  const containerGame =document.querySelector(".container")
+  const containerGame = document.querySelector(".container");
   containerGame.append(choose, btn1, btn2, btn3);
-  const link1 = document.createElement("a");
-  btn1.append(link1);
 
   //Adding active state to clicked button
+  const buttons = [btn1, btn2, btn3];
   function changeColor() {
+    buttons.map((button) => button.classList.remove("active"));
     this.classList.toggle("active");
   }
   //  btn1.addEventListener('click', changeColor)
-  const buttons = [btn1, btn2, btn3];
   buttons.map((button) => button.addEventListener("click", changeColor));
 };
