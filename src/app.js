737959ac3e9882ba5219efa7600c3f0ac59c86a2
Ewelina Mężyk <ewelinamezykmail@gmail.com>
2021-12-10 17:36:25 +0000

    filtering out character data

--- src/app.js
+++ src/app.js
@@ -1,24 +1,36 @@
 import { createHallOfFame } from "./components/hall_of_fame/hall_of_fame";
 import { createTimer } from "./components/timer/timer";
 import clickLogo from "./components/logo/logo";
-import {apiAccess} from "./components/API/api";
-import {randomFourCharacters} from "./components/API/random";
-import {newQuestion, Question, PHOTO_MODE, NAME_MODE, FAMILY_NAME_MODE} from "./components/API/newQuestion";
-
+import { apiAccess } from "./components/API/api";
+import { randomFourCharacters } from "./components/API/random";
+import {
+  newQuestion,
+  Question,
+  PHOTO_MODE,
+  NAME_MODE,
+  FAMILY_NAME_MODE,
+} from "./components/API/newQuestion";
 
 const app = () => {
   clickLogo();
   createTimer();
   createHallOfFame();
   apiAccess()
-    .then(res => {
-      localStorage.setItem("characters", JSON.stringify(res));
-      const currentQuestion = newQuestion(NAME_MODE)
-      console.log(currentQuestion)
+    .then((fullCharacters) => {
+      const characters = fullCharacters.map((character) => {
+        return {
+          family: character.family,
+          id: character.id,
+          imageURL: character.imageUrl,
+          fullName: character.fullName,
+        };
+      });
+
+      localStorage.setItem("characters", JSON.stringify(characters));
+      const currentQuestion = newQuestion(NAME_MODE);
+      console.log(currentQuestion);
     })
-    .catch(err => console.error(err))
+    .catch((err) => console.error(err));
 };
 
-export default app
-
-
+export default app;
