a77dc61a94425a0bd1a45988987ade2b7ea10368
Vivienne <wioleta.porosa@onet.pl>
2021-12-13 20:15:11 +0100

    Deleted x-sign and added buttonRules

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import { createHallOfFame } from "./components/hall_of_fame/hall_of_fame";
 import { createTimer } from "./components/timer/timer";
 import clickLogo from "./components/logo/logo";
 import {gameRules} from "./components/gameRules/gameRules";
+import {btn} from "./components/buttonRules/buttonRules";
 const app = () => {
   // clickLogo();
   // createTimer();
   // createHallOfFame();
   gameRules();
+  btn();
 };
 
 export default app
--- src/components/gameRules/gameRules.js
+++ src/components/gameRules/gameRules.js
@@ -3,22 +3,6 @@ const game = document.createElement('div');
 game.classList.add('rules');
 document.getElementById("app").appendChild(game);
 
-const exit = document.createElement('div');
-exit.classList.add('exit');
-game.appendChild(exit);
-
-const circle = document.createElement('div');
-circle.classList.add('circle');
-exit.appendChild(circle);
-
-const bar1 =document.createElement('div');
-bar1.classList.add('bar1');
-circle.appendChild(bar1);
-
-const bar2 =document.createElement('div');
-bar2.classList.add('bar2');
-circle.appendChild(bar2);
-
 const header = document.createElement('div');
 header.classList.add('header');
 game.appendChild(header);
@@ -32,7 +16,7 @@ ruleName1.textContent='Character - name';
 game.appendChild(ruleName1);
 
 const ruleDesc1 = document.createElement('p');
-ruleDesc1.textContent='During the quiz, you will have to select the correct picture of the character whose name is displayed on the screen. The game ends after 1minutes or after you answer 20 questions.';
+ruleDesc1.textContent='During the quiz, you will have to select the correct picture of the character whose name is displayed on the screen. The game ends after 1 minutes or after you answer 20 questions.';
 game.appendChild(ruleDesc1);
 
 const ruleName2 = document.createElement('h2');
@@ -48,6 +32,7 @@ ruleName3.textContent='Character - family';
 game.appendChild(ruleName3);
 
 const ruleDesc3 = document.createElement('p');
-ruleDesc3.textContent='During the quiz, you will have to guess the name of the character’s family displayed in the picture. The game ends after 1 minutes or after you answer 20 questions.';
+ruleDesc3.textContent='During the quiz, you will have to guess the name of the character\’s family displayed in the picture. The game ends after 1 minutes or after you answer 20 questions.';
 game.appendChild(ruleDesc3);
-}
+};
+
