86a0dd37e408350665e4e043b0f8c2863ef4d1f5
Luke_Z <zurawskiu@gmail.com>
2021-12-17 21:21:27 +0100

    Disable eslint rule prefer default export. Add pre-commit hook.

--- src/app.js
+++ src/app.js
@@ -5,35 +5,19 @@ import { createTimer } from "./components/timer/timer";
 import clickLogo from "./components/logo/logo";
 import { gameRules } from "./components/gameRules/gameRules";
 import { btn } from "./components/buttonRules/buttonRules";
-import { apiAccess } from "./components/API/api";
-import {
-  newQuestion,
-  PHOTO_MODE,
-  NAME_MODE,
-  FAMILY_NAME_MODE,
-} from "./components/API/newQuestion";
-import {
-  storeRankingScores,
-  GamePlayer,
-} from "./components/LocalStorageScores/LocalStorageScores";
-
-let availableIds;
-await apiAccess().then((ids) => (availableIds = ids));
 
 const app = () => {
   startButton();
   menuCreator();
-  getCurrentGameMode((mode) => console.log(mode));
+  getCurrentGameMode();
   clickLogo();
   createTimer();
   createHallOfFame();
-  newQuestion(PHOTO_MODE, availableIds).then((currentQuestion) =>
-    console.log(currentQuestion)
-  );
-  // gameRules();
-  // btn();
-  let player = new GamePlayer("Ewelina Mężyk", 20);
-  storeRankingScores(FAMILY_NAME_MODE, player);
+  // newQuestion(PHOTO_MODE, availableIds).then((currentQuestion) =>
+  //   console.log(currentQuestion)
+  // );
+  gameRules();
+  btn();
 };
 
 export default app;
