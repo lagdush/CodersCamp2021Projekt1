af90b46dcb3165e960c9a9d2186d6197d0158436
Luke_Z <zurawskiu@gmail.com>
2022-01-01 20:43:07 +0100

    fix

--- src/components/Homepage_image/Homepage_image.js
+++ src/components/Homepage_image/Homepage_image.js
@@ -3,7 +3,7 @@ import { PHOTO_MODE, NAME_MODE, FAMILY_NAME_MODE } from "../API/newQuestion";
 
 export const homePagePoster = () => {
   const poster = document.createElement("img");
-  poster.setAttribute("src", "../../image/got_module1_photo.png");
+  poster.setAttribute("src", "src/image/got_module1_photo.png");
   poster.classList.add("poster");
   poster.setAttribute("id", "poster");
   document.getElementById("app").appendChild(poster);
@@ -15,13 +15,13 @@ const switchPhotoMode = () => {
   getCurrentGameMode((mode) => {
     switch (mode) {
       case PHOTO_MODE:
-        poster.src = "../../image/got_module1_photo.png";
+        poster.src = "src/image/got_module1_photo.png";
         break;
       case NAME_MODE:
-        poster.src = "../../../src/image/got_module2_photo.jpg";
+        poster.src = "src/src/image/got_module2_photo.jpg";
         break;
       case FAMILY_NAME_MODE:
-        poster.src = "../../../src/image/got_module3_photo.jpg";
+        poster.src = "src/src/image/got_module3_photo.jpg";
         break;
       default:
         throw new Error("Unknown game mode");
