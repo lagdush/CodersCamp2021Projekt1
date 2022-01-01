0ef1804c479793e204e6070a6e3c082e18a7a623
Luke_Z <zurawskiu@gmail.com>
2022-01-01 20:52:13 +0100

    last one

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,6 +1,6 @@
 export default function clickLogo() {
   const logo = document.createElement("img");
-  logo.setAttribute("src", "../../../src/image/logo.png");
+  logo.setAttribute("src", "./src/image/logo.png");
   logo.classList.add("logo");
   document.getElementById("app").appendChild(logo);
   logo.addEventListener("click", () => window.location.reload());
