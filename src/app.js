d74d702907c000ba2d9a51eae0e4c6a6a0de80c0
Luke_Z <zurawskiu@gmail.com>
2022-01-02 18:52:55 +0100

    work

--- src/app.js
+++ src/app.js
@@ -1,69 +1,65 @@
-import { startButton } from "./components/startButton";
-import { menuCreator, getCurrentGameMode } from "./components/gameMenu";
-import {
-  GamePlayer,
-  storeRankingScores,
-} from "./components/LocalStorageScores/LocalStorageScores";
-import clickLogo from "./components/logo/logo";
-import {
-  homePagePoster,
-  changePosterByMode,
-} from "./components/Homepage_image/Homepage_image";
-import { apiAccess } from "./components/API/api";
-let availableIds;
-await apiAccess().then((ids) => (availableIds = ids));
-import {
-  HumanPlayer,
-  createPlayer,
-} from "./components/LogicHumanPlayer/LogicHumanPlayer";
-import {
-  FAMILY_NAME_MODE,
-  newQuestion,
-  PHOTO_MODE,
-  NAME_MODE,
-} from "./components/API/newQuestion";
-import { modalWindow } from "./components/modal_window/modalWindow";
-import { btn } from "./components/buttonRules/buttonRules";
-import { menuView } from "./components/MainMenu/MainMenu";
-import { btnHallOfFame } from "./components/buttonHallOfFame/buttonHallOfFame";
+// import { startButton } from "./components/startButton";
+// import { menuCreator, getCurrentGameMode } from "./components/gameMenu";
+// import {
+//   GamePlayer,
+//   storeRankingScores,
+// } from "./components/LocalStorageScores/LocalStorageScores";
+// import clickLogo from "./components/logo/logo";
+// import {
+//   homePagePoster,
+//   changePosterByMode,
+// } from "./components/Homepage_image/Homepage_image";
+// import { apiAccess } from "./components/API/api";
+// import {
+//   HumanPlayer,
+//   createPlayer,
+// } from "./components/LogicHumanPlayer/LogicHumanPlayer";
+// import {
+//   FAMILY_NAME_MODE,
+//   newQuestion,
+//   PHOTO_MODE,
+//   NAME_MODE,
+// } from "./components/API/newQuestion";
+// import { modalWindow } from "./components/modal_window/modalWindow";
+// import { btn } from "./components/buttonRules/buttonRules";
+// import { menuView } from "./components/MainMenu/MainMenu";
+// import { btnHallOfFame } from "./components/buttonHallOfFame/buttonHallOfFame";
+
+// let availableIds;
+// await apiAccess().then((ids) => (availableIds = ids));
 
 const app = () => {
-  startButton();
-  menuCreator();
-  getCurrentGameMode();
-  clickLogo();
-  // createTimer();
-  // createHallOfFame();
-  newQuestion(PHOTO_MODE, availableIds).then((currentQuestion) =>
-    console.log(currentQuestion)
-  );
-  getCurrentGameMode((mode) => console.log(mode));
-  btn();
-  btnHallOfFame();
-  let player = new GamePlayer("Ewelina Mężyk", 20);
-  storeRankingScores(FAMILY_NAME_MODE, player);
-  homePagePoster();
-  changePosterByMode();
-  menuView();
-
-  const currentQuestionPromise = newQuestion(FAMILY_NAME_MODE, [1, 2, 3, 4]);
-  currentQuestionPromise
-    .then((currentQuestion) => {
-      const player = createPlayer();
-
-      player.askQuestion(currentQuestion, () => console.log("question asked!"));
-
-      player.answer(currentQuestion.charactersToChoseFrom[0], () =>
-        console.log("player has answered!")
-      );
-
-      const answersCount = player.countAnswers();
-      console.log("Total answers: " + answersCount);
-
-      const correctAnswersCOunt = player.countCorrectAnswers();
-      console.log("Correct answers: " + correctAnswersCOunt);
-    })
-    .catch((e) => console.log(e));
+  // startButton();
+  // menuCreator();
+  // getCurrentGameMode();
+  // clickLogo();
+  // // createTimer();
+  // // createHallOfFame();
+  // newQuestion(PHOTO_MODE, availableIds).then((currentQuestion) =>
+  //   console.log(currentQuestion)
+  // );
+  // getCurrentGameMode((mode) => console.log(mode));
+  // btn();
+  // btnHallOfFame();
+  // const player = new GamePlayer("Ewelina Mężyk", 20);
+  // storeRankingScores(FAMILY_NAME_MODE, player);
+  // homePagePoster();
+  // // changePosterByMode();
+  // menuView();
+  // const currentQuestionPromise = newQuestion(FAMILY_NAME_MODE, [1, 2, 3, 4]);
+  // currentQuestionPromise
+  //   .then((currentQuestion) => {
+  //     const player = createPlayer();
+  //     player.askQuestion(currentQuestion, () => console.log("question asked!"));
+  //     player.answer(currentQuestion.charactersToChoseFrom[0], () =>
+  //       console.log("player has answered!")
+  //     );
+  //     const answersCount = player.countAnswers();
+  //     console.log(`Total answers: ${answersCount}`);
+  //     const correctAnswersCOunt = player.countCorrectAnswers();
+  //     console.log(`Correct answers: ${correctAnswersCOunt}`);
+  //   })
+  //   .catch((e) => console.log(e));
 };
 
 export default app;
--- src/components/hall_of_fame/hall_of_fame.js
+++ src/components/hall_of_fame/hall_of_fame.js
@@ -1,26 +1,37 @@
-export const createHallOfFame = () => {
-  const wrapper = document.createElement("div");
-  wrapper.classList.add("wrapper");
-  const header = document.createElement("div");
-  header.classList.add("ranking");
-  const tigerIcon = document.createElement("img");
-  tigerIcon.setAttribute("src", "../photos/podium.png");
-  const rankingHeader = document.createElement("p");
-  rankingHeader.innerText = "Mode Ranking";
+// /* eslint-disable no-restricted-syntax */
+// import { checkScoresInLocalStorage } from "../LocalStorageScores/LocalStorageScores";
 
-  const place = document.createElement("p");
-  place.textContent = "Place";
-  wrapper.append(place);
+// export const createHallOfFame = () => {
+//   const ls = checkScoresInLocalStorage();
+//   console.log(ls.familyNameMode.scores);
+//   const list = document.createElement("ol");
+//   const listItem = document.createElement("li");
+//   const wrapper = document.createElement("div");
+//   wrapper.classList.add("wrapper");
+//   const header = document.createElement("div");
+//   header.classList.add("ranking");
+//   const tigerIcon = document.createElement("img");
+//   tigerIcon.setAttribute("src", "../photos/podium.png");
+//   const rankingHeader = document.createElement("p");
+//   rankingHeader.innerText = "Mode Ranking";
 
-  const player = document.createElement("p");
-  player.textContent = "Player";
-  wrapper.append(player);
+//   const place = document.createElement("p");
+//   place.textContent = "Score";
+//   wrapper.append(place);
 
-  const answers = document.createElement("p");
-  answers.textContent = "Answers";
-  wrapper.append(answers);
+//   const player = document.createElement("p");
+//   player.textContent = "Player";
+//   wrapper.append(player);
+//   for (const hofPlayer of ls.familyNameMode.scores) {
+//     place.append((document.createElement("p").textContent = hofPlayer.score));
+//     player.append((document.createElement("p").textContent = hofPlayer.name));
+//   }
 
-  document.getElementById("app").appendChild(wrapper);
-  wrapper.append(header);
-  header.append(tigerIcon, rankingHeader);
-};
+//   // const answers = document.createElement("p");
+//   // answers.textContent = "Answers";
+//   // wrapper.append(answers);
+
+//   document.getElementById("app").appendChild(wrapper);
+//   wrapper.append(header);
+//   header.append(tigerIcon, rankingHeader);
+// };
